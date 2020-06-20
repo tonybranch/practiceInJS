df3103c4ea11567a404ac3337c8fd059a347bd80
Tony Branch <tonybranch.org@yahoo.com>
2020-06-20 19:28:26 +0300

    Practice in Vanilla JS.

--- prototype/index.js
+++ prototype/index.js
@@ -1,9 +1,115 @@
 'use strict'
 
-const person = {
-  name: 'Tony',
-  age: 35,
-  greet: function() {
-    console.log('Greet!')
-  }
-}
+const arrow = (a, b) => a + b
+console.log(arrow(5, 8));
+
+// let counter = 0
+// const interval = setInterval(function() {
+//   if(counter === 5) {
+//     clearInterval(interval)
+//   } else {
+//     console.log(++counter)
+//   }
+// }, 1000)
+
+// function nameInput(name, age) {
+//   console.log(`Name user: ${name}, ${age}`);
+// }
+
+// nameInput('Tony', 35)
+
+// const arrow = (name) => {
+//   console.log(`Name user: ${name}`);
+// }
+
+// arrow('Branch')
+
+// function sumAll(...all) {
+//   console.log(all)
+//   let count = 0
+//   all.forEach(element => {
+//     count = count + element
+//     // console.log(count);
+//   });
+//   console.log(count);
+// }
+
+// sumAll(1,2,3,4,5)
+
+// function sumAll2(...all) {
+//   console.log(all);
+//   let cmd = 0
+//   for(let num of all) {
+//     cmd += num
+//   }
+//   console.log(cmd);
+// }
+
+// sumAll2(2,4,6,8)
+
+
+// function hello() {
+//   const data = 'World'
+//   console.log(`Hello, ${data}!`)
+// }
+
+// const hello2 = function hello() {
+//   const data = 'World2'
+//   console.log(`Hello2, ${data}!`)
+// }
+
+// hello()
+// hello2()
+
+// console.log(typeof hello);
+// console.log(typeof hello2);
+
+// console.dir(hello)
+
+
+
+// let counter = 0
+
+// const interval = setInterval(function() {
+//   if (counter === 5) {
+//     clearInterval(interval)
+//   } else {
+//     console.log(++counter)
+//   }
+// }, 1000)
+
+// const name = 'Tony'
+// console.log(name.startsWith('T'))
+// console.log(name);
+
+// function logPerson(s, name, age) {
+//   console.log(s, name, age)
+//   return `${s[0]}${name}${s[1]}${age}${s[2]}`
+// }
+
+// const personNane = 'Tony'
+// const personAge = 26
+// const output = logPerson`Имя: ${personNane}, Возраст: ${personAge}!`
+
+// console.log(output)
+
+// const person = {
+//   firstName: 'Tony',
+//   lastName: 'Branch',
+//   year: 1985,
+//   prnt: function() {
+//     console.log('Print text - person')
+//   }
+// }
+
+// person.prnt()
+// console.log(person.firstName, person.lastName, person.year.toString())
+
+// console.log(person['year'])
+// const key = 'lastName'
+// console.log(person[key])
+
+// person.goGo = true
+// console.log(person)
+// console.log(person.goGo)
+
